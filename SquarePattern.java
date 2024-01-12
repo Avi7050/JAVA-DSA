2a1802c405be99b795ace147269e9b2b970006f8
Avi7050 <avi7050416623@gmail.com>
2024-01-12 09:59:12 +0530

    files modified

--- SquarePattern.java
+++ SquarePattern.java
@@ -1,35 +1,67 @@
-class SquarePattern{
-    static void pattern1(int n){
-        for(int i = 0;i<n;i++){
-            for(int j=0;j<n;j++){
+class SquarePattern {
+    public static void pattern1(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
-    public static void pattern2(int n){
+
+    public static void pattern2(int n) {
         int num = 1;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(num+" ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(num + " ");
             }
             num++;
             System.out.println();
         }
     }
-    public static void pattern3(int n){
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(j+" ");
+
+    public static void pattern3(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(j + " ");
             }
-            
             System.out.println();
         }
     }
+
+    public static void pattern4(int n) {
+        char ch = 'A';
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(ch + " ");
+                ch = (char) (ch + 1);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern5(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i == 0 || j == 0 || i == (n - 1) || j == (n - 1)){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
+        System.out.println("Printing *");
         pattern1(5);
+        System.out.println("Printing Numbers");
         pattern2(5);
+        System.out.println("Printing numbers in seq");
         pattern3(5);
+        System.out.println("Printing Alphabet");
+        pattern4(6);
+        System.out.println("Printing Hollow Square");
+        pattern5(5);
     }
 }
